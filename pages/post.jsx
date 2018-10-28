dfd28ac29319df83eae405c23a3465a88cde1381
Takeshi <iwatakeshi@gmail.com>
2018-10-28 12:22:04 -0600

    Revert "Add react-markdown"
    This reverts commit e0a0a70aadfa47729234f47c3f5e4492dfc3ec9a.

--- pages/post.jsx
+++ pages/post.jsx
@@ -1,40 +1,23 @@
-import Layout from '../components/Layout'
-import {withRouter} from 'next/router'
-import Markdown from 'react-markdown'
+import { withRouter } from 'next/router';
+import Layout from '../components/Layout';
 
-export default withRouter((props) => (
-  <Layout>
-   <h1>{props.router.query.title}</h1>
-   <div className="markdown">
-     <Markdown source={`
-This is our blog post.
-Yes. We can have a [link](/link).
-And we can have a title as well.
 
-### This is a title
+const Post = (props) => (
+    <Layout>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p/g, '')}</p>
+        <img src={props.show.image.medium} />
+    </Layout>
+)
 
-And here's the content.
-     `}/>
-   </div>
-   <style jsx global>{`
-     .markdown {
-       font-family: 'Arial';
-     }
+Post.getInitialProps = async function(context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
 
-     .markdown a {
-       text-decoration: none;
-       color: blue;
-     }
+    console.log(`Fetched show: ${show.name}`);
 
-     .markdown a:hover {
-       opacity: 0.6;
-     }
+    return { show };
+}
 
-     .markdown h3 {
-       margin: 0;
-       padding: 0;
-       text-transform: uppercase;
-     }
-  `}</style>
-  </Layout>
-))
+export default Post;
