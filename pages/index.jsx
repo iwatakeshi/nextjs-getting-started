2210c1b482422f85c42781a1431aaab6b84af9d3
Takeshi <iwatakeshi@gmail.com>
2018-10-28 12:26:25 -0600

    Fix styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,46 @@
 import Layout from '../components/Layout.js'
 import Link from 'next/link'
 
-function getPosts () {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js'},
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
-  ]
+function getPosts() {
+    return [
+        { id: 'hello-nextjs', title: 'Hello Next.js' },
+        { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+        { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
+    ]
 }
 
 const PostLink = ({ post }) => (
-  <li>
-    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-      <a>{post.title}</a>
-    </Link>
-  </li>
+    <li>
+        <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+            <a>{post.title}</a>
+        </Link>
+        <style jsx>{`
+        li {
+            list-style: none;
+            margin: 5px 0;
+        }
+
+        a {
+            text-decoration: none;
+            color: blue;
+        }
+
+        a:hover {
+            opacity: 0.6;
+        }
+    `}</style>
+    </li>
 )
 
 export default () => (
-  <Layout>
-    <h1>My Blog</h1>
-    <ul>
-      {getPosts().map((post) => (
-        <PostLink key={post.id} post={post}/>
-      ))}
-    </ul>
-    <style jsx>{`
+    <Layout>
+        <h1>My Blog</h1>
+        <ul>
+            {getPosts().map((post) => (
+                <PostLink key={post.id} post={post} />
+            ))}
+        </ul>
+        <style jsx>{`
       h1, a {
         font-family: "Arial";
       }
@@ -33,20 +48,6 @@ export default () => (
       ul {
         padding: 0;
       }
-
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
     `}</style>
-  </Layout>
+    </Layout>
 )
